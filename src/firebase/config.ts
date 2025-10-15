f96a4e08d3ef161b27a88edd37fe0cdda8fb6480
Damisile Ayoola <damisileayoola@gmail.com>
2025-10-15 17:04:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2162100590-3cc10",
+  "appId": "1:158983679431:web:1886d5e6b16615ac3dbde2",
+  "apiKey": "AIzaSyCwNkFbS02y6gOqDYQ_x89EfJJLycYjv1U",
+  "authDomain": "studio-2162100590-3cc10.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "158983679431"
+};
